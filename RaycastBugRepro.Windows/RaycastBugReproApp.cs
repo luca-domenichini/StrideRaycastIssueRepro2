fa8909869b464c1e94fe86cb938c67dfb2f927b5
Luca Domenichini <luca.domenic@gmail.com>
2025-04-07 00:18:55 +0200

    Updated `RaycastBugRepro.Windows.csproj` to set output type to `Exe`, converting the application to a console app. Added logging to provide instructions for developers

--- RaycastBugRepro.Windows/RaycastBugReproApp.cs
+++ RaycastBugRepro.Windows/RaycastBugReproApp.cs
@@ -1,5 +1,30 @@
 using Stride.Engine;
+using System;
+using System.Diagnostics;
+using System.Threading;
 
 using var game = new Game();
 game.Run();
 
+Console.WriteLine("Game finished! Here are the threads running:");
+
+// print out the list of threads running
+foreach (ProcessThread thread in Process.GetCurrentProcess().Threads)
+{
+    Console.WriteLine($"Thread ID: {thread.Id}, State: {thread.ThreadState}");
+}
+
+game.Dispose(); // try to set a breakpoint here and look at Visual Studio Threads window, before and after disposing.
+
+Thread.Sleep(1000); // wait for a second to give CLR some time to clean up the threads.. (we should not need that)
+
+Console.WriteLine("Game disposed! Here are the remaining threads:");
+
+// after the game is disposed, print out the list of threads running
+foreach (ProcessThread thread in Process.GetCurrentProcess().Threads)
+{
+    Console.WriteLine($"Thread ID: {thread.Id}, State: {thread.ThreadState}");
+}
+
+Console.WriteLine("Game disposed. Press any key to exit."); // set a breakpoint here and look at Visual Studio Threads window. There are still threads running from BepuUtilities and Stride ThreadPool
+Console.ReadKey();
